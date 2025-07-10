3934e0bee6f9ddf0eee820425554dd424eb724ae
tushar-gleecus <tushar.canchi@gleecus.com>
2025-07-10 16:10:03 +0530

    Update HomeScreen.jsx

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.svg';
+import logo from '../assets/new_logo.png';
 
 const HomeScreen = () => {
   const navigate = useNavigate();
